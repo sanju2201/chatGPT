11503990a4b9a241c033c43c9ed32d486bf7d5ce
SanjuManna <sanjumanna2201@gmail.com>
2023-06-04 12:25:28 +0530

    new Commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>Chat GPT</h1>
+      <h2>New</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require('body-parser');
+const colors = require("colors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+// dotenv
+dotenv.config();
+
+// mongo connection
+connectDB();
+
+
+// Line
+
+// rest object
+const app = express();
+
+// Adding middlewares
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
+const PORT = process.env.PORT || 8080;
+// Listen server
+app.listen(PORT, () => {
+    console.log(`Server running on PORT on ${PORT} in ${process.env.DEV_MODE} mode`.bgCyan.white);
+})
